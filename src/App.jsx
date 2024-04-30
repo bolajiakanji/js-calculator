2aaa60455820390803ac6311a146e9ea505b9e7d
lukman bolaji <bolajiakanji21@gmail.com>
2024-04-29 22:51:57 -0700

    add number button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [displaystate, setDisplaystate] = useState("gd");
+//  useEffect(() => {
+//     let buttonNumbers = document.querySelectorAll(".button_number");
+//     let button 
+//     for (button=0;button<buttonNumbers.length; button++){
+//       buttonNumbers[button].addEventListener("click",you)
+
+//     }
+
+    
+     
+//     return () => {
+//       for (button=0;button<buttonNumbers.length; button++){
+        
+//       buttonNumbers[button].removeEventListener("click",you)}}
+
+         
+
+    
+//     });
+
+//     function you(e) {
+        
+//       console.log('loop')
+//       setDisplaystate(displaystate + e.target.innerText )
+//     }  
+
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="container">
+      <div id="display">{displaystate}</div>
+      <div id="clear">AC</div>
+      <div className="button_number" id="divide">
+        /
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="button_number" id="multiply">
+        x
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className="button_number" id="seven" >
+        7
+      </div>
+      <div className="button_number" id="eight">
+        8
+      </div>
+      <div className="button_number" id="nine">
+        9
+      </div>
+      <div className="button_number" id="subtract">-</div>
+
+      <div className="button_number" id="four">
+        4
+      </div>
+      <div className="button_number" id="five">
+        5
+      </div>
+      <div className="button_number" id="six">
+        6
+      </div>
+      <div className="button_number" id="add">+</div>
+
+      <div className="button_number" id="one">
+        1
+      </div>
+      <div className="button_number" id="two">
+        
+      </div>
+      <div className="button_number" id="three">
+        3
+      </div>
+      <div className="button_number" id="equals">=</div>
+      <div className="button_number" id="zero">
+        0
+      </div>
+      <div className="button_number" id="decimal">
+        
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
